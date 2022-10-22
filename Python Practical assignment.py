61246853d3d7e370d9b8c1880d9773d475a13c5b
avina <avinashgv239@gmail.com>
2022-10-22 15:17:20 +0530

    Python Practical assignment 1

---
+++ Python Practical assignment.py
@@ -0,0 +1,41 @@
+
+#1. Write a Python program to print "Hello Python"?
+print("Hello Python")
+
+#2. Write a Python program to do arithmetical operations addition and division.?
+A=10
+B=5
+addition=A+B
+Division=A/B
+print("Addition of two number:",addition )
+print("Division of two number:",Division )
+
+#3. Write a Python program to find the area of a triangle?
+Base= 30
+Height=15
+Area= 1/2*(Base*Height)
+print("Area of a Triangle:", Area)
+
+#4. Write a Python program to swap two variables?
+X=10
+Y=12
+
+print("Befor swaping:")
+print("variable X:", X)
+print("variable X:", Y)
+
+X,Y=Y,X
+
+print("After swaping:")
+print("variable X:", X)
+print("variable X:", Y)
+
+#5. Write a Python program to generate a random number?
+
+import random
+
+print(random.randint(1,100))
+
+
+
+
